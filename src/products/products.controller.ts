cbb1172d4b3206f7bae0334904f658c0689721b2
lfrz <anonimus47@hotmail.com>
2023-12-30 11:09:49 -0500

    Finalizando integracion con TypeORM v

---
+++ src/products/products.controller.ts
@@ -0,0 +1,63 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseUUIDPipe,
+  NotFoundException,
+  Query,
+} from '@nestjs/common';
+
+import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  async create(@Body() createProductDto: CreateProductDto) {
+    return await this.productsService.create(createProductDto);
+  }
+
+  @Get()
+  async findAll(@Query() paginationDto: PaginationDto) {
+    return await this.productsService.findAll(paginationDto);
+  }
+
+  @Get(':term')
+  async findOne(@Param('term') term: string) {
+    const product = await this.productsService.findOne(term);
+
+    return product;
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    return this.productsService.update(id, updateProductDto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id', ParseUUIDPipe) id: string) {
+    const product = await this.GetProductById(id);
+
+    return await this.productsService.remove(product);
+  }
+
+  private async GetProductById(id: string) {
+    const product = await this.productsService.findOne(id);
+
+    if (!product) {
+      throw new NotFoundException(`Product with id: ${id} not found v!`);
+    }
+    return product;
+  }
+}
